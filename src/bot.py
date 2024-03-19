1f2492e646530c1863a86d9e64c6ce922ea5bc16
williamobiana <williamobiana@gmail.com>
2024-03-19 21:29:49 +0000

    Initial commit

---
+++ src/bot.py
@@ -0,0 +1,14 @@
+import boto3
+import os
+
+bucket = os.environ.get('DestinationBucketName')
+
+def lambda_handler(event, context):
+
+    # Upload the file
+    data = b'Hello World'
+    client = boto3.client('s3')
+    response = client.put_object(Body=data, Bucket=bucket, Key='raw_folder/filename.txt')
+    
+    # return
+    return True
