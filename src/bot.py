1c94f6a920e5ba897b4a8ecce09b57d8110a2ecb
williamobiana <williamobiana@gmail.com>
2024-03-21 00:03:18 +0000

    test

--- src/bot.py
+++ src/bot.py
@@ -5,6 +5,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "crawl test",
+            "message": "running crawl test",
         }),
     }
