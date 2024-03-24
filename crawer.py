900a1e2303c96a45e8c1f21b2dad02a571c0a8e5
Obiana-Ifeanyi <ifeanyiobiana@gmail.com>
2024-03-24 21:45:27 +0100

    updated crawer file

--- crawer.py
+++ crawer.py
@@ -1,11 +1,12 @@
 import time
+import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-output_file = "prettified_output.txt"
+output_file = "output_test.txt"
 
 options = Options()
 # driver = webdriver.Chrome('/home/gris/Downloads/chromedriver-linux64/chromedriver')  # Optional argument, if not specified will search path.
@@ -16,11 +17,23 @@ driver.get('https://www.sportybet.com/ng/sport/football/today');
 
 soup = BeautifulSoup(driver.page_source, "lxml")
 
-print (soup.text)
+# print (soup.text)
 
-with open(output_file, "w") as f:
-    f.write(soup.text)
+with open(output_file, "w") as fw:
+    fw.write(soup.text)
 
+
+with open(output_file, "r") as fr:
+    content = fr.read()
+
+# Define the regular expression pattern
+regex_pattern = r'\b\d{2}/\d{2}\s(?:Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)\b'
+
+# Find all matches using the regular expression
+days = re.findall(regex_pattern, content)
+
+for day in days:
+    print (day)
 
 time.sleep(5) # Let the user actually see something!
 
