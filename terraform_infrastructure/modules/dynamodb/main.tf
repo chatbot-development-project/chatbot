9383f6c0438101060e97f0c6b48e0823b1b13b34
Obiana Ifeanyi <Ifeanyiobiana@gmail.com>
2024-04-25 13:35:29 +0100

    Add files via upload
    removed the iam folder and put the policy folder in the module

--- terraform_infrastructure/modules/dynamodb/main.tf
+++ terraform_infrastructure/modules/dynamodb/main.tf
@@ -1,9 +1,40 @@
-resource "aws_dynamodb_table" "example" {
-  name           = "example"
-  hash_key       = "id"
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "user_metadata"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "message_id"
+  range_key      = "timestamp"
   stream_enabled = true
   stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "message_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+  global_secondary_index {
+    name               = "UserTitleIndex"
+    hash_key           = "message_id"
+    range_key          = "timestamp"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "KEYS_ONLY"  # Corrected projection_type
+    non_key_attributes = []
+  }
+
+  tags = {
+    Name        = "dynamodb-table"
+    Environment = "Prod"
+  }
 }
